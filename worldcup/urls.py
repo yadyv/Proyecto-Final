c1a3b6859963ce6a9aa0b9fdaa54c8789122a326
yadyv <andreavarelab@hotmail.com>
2014-04-04 00:52:43 +0200

    Update urls.py

--- worldcup/urls.py
+++ worldcup/urls.py
@@ -1,12 +1,28 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+import equipos.views
+
 admin.autodiscover()
-
+ 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'worldcup.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+# Examples:
+# url(r'^blog/', include('blog.urls')),
+url(r'^continentes/', equipos.views.ListarContinentes.as_view(), name='lista-continentes',),
+url(r'^jugadores/', equipos.views.ListarJugadores.as_view(), name='lista-jugadores',),
+url(r'^equipos/', equipos.views.ListarEquipos.as_view(), name='listar-equipos',),
+url(r'^player/(?P<jugador_id>\d+)/player.html$',
+'equipos.views.detalleJugador', name='detalle-jugador'),
+ 
+url(r'^admin/', include(admin.site.urls)),
+url(r'^media/(?P<path>.*)$','django.views.static.serve',
+            {'document_root':settings.MEDIA_ROOT,}
+   ),
 
-    url(r'^admin/', include(admin.site.urls)),
 )
+
+
+ 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
