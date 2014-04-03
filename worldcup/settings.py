97468ec059b405d7e03756e93e118a5615958a12
yadyv <andreavarelab@hotmail.com>
2014-04-04 00:56:27 +0200

    Update settings.py

--- worldcup/settings.py
+++ worldcup/settings.py
@@ -81,3 +81,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/static/')
+STATICFILES_DIRS= ('/home/developer/worldcup/static',)
+MEDIA_ROOT= os.path.join(BASE_DIR,'media/')
+MEDIA_URL = '/media/'
